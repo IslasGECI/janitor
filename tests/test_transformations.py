0342d58325f739617d45fbd698c9123f9d7927de
Mario Villasante <mario.villasante@islas.org.mx>
2023-06-20 16:57:44 -0700

    🔥 Remove dummy tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,9 +1,2 @@
-import janitor as dt
-
-
 def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
+    pass
