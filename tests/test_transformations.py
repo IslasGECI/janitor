5a2232d4f4850dede30266e1ee5446d34186cb87
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-10 17:47:43 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,2 +1,13 @@
-def test_add_offset():
-    pass
+from janitor import janitor
+
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_app():
+    result = runner.invoke(
+        janitor,
+        ["transform-cat-data", "--help"],
+    )
+    assert result.exit_code == 0
