325d28539afee6b16056b6a0913fba5c8a147b38
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-10 18:08:20 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,5 +1,5 @@
 from janitor import janitor
-
+import os
 from typer.testing import CliRunner
 
 runner = CliRunner()
@@ -11,3 +11,15 @@ def test_app():
         ["transform-cat-data", "--help"],
     )
     assert result.exit_code == 0
+    expected_file_path = "tests/data/IG_POSICION_TRAMPAS_02JUL2023_clean.csv"
+    if os.path.exist(expected_file_path):
+        os.remove(expected_file_path)
+
+    folder_with_data_path = "tests/data"
+    os.chdir(folder_with_data_path)
+    result = runner.invoke(
+        janitor,
+        ["transform-cat-data"],
+    )
+    os.chdir("/workdir")
+    assert os.path.exist(expected_file_path)
