2d02211834ea395297cef5dc8d434c2dca8d0043
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-12 21:36:10 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,12 @@ runner = CliRunner()
 def test_app():
     result = runner.invoke(
         janitor,
+        ["write-active-traps-summary", "--help"],
+    )
+    assert result.exit_code == 0
+
+    result = runner.invoke(
+        janitor,
         ["transform-cat-data", "--help"],
     )
     assert result.exit_code == 0
